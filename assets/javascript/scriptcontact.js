9334d1c7a21f586511b02ad57d79fd237f98eaf5
Cora203 <92077468+Cora203@users.noreply.github.com>
2023-01-10 16:23:12 +0100

    Add files via upload

--- assets/javascript/scriptcontact.js
+++ assets/javascript/scriptcontact.js
@@ -1,11 +1,11 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// Ths function will check if the form is being filled
 (function () {
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
+  //This array will chek every input if it is valid or not
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
